ab7e3fb6ac79726a0df6dcc009136e7f13c90046
Jj87-huy <haoallain964@gmail.com>
2026-03-02 16:08:58 +0000

    update

---
+++ systems/logger/index.js
@@ -0,0 +1,8 @@
+const { ensure } = require('../../core/modulesManager');
+const sender = require('./sender');
+const types = require('./types');
+
+module.exports = {
+    onload(client) {},
+    onUnload(client) {}
+};
